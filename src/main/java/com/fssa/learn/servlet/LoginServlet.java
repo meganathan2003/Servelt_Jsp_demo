d1c9f75754d1a647da7430e2fc045bc8f5f3f9b6
Meganathan Subramanian <mesubramanian@fssa.freshworks.com>
2023-08-27 12:14:44 +0530

    Alter the servlet code

--- src/main/java/com/fssa/learn/servlet/LoginServlet.java
+++ src/main/java/com/fssa/learn/servlet/LoginServlet.java
@@ -2,7 +2,6 @@ package com.fssa.learn.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,51 +13,35 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LoginServlet
  */
-
-@WebServlet("/LoginServlet")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-	
-	
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public LoginServlet() {
-		super();
-
-	}
-	
-	
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException 
-			
-	{
-		response.setContentType("text/html");
+       
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String userName = request.getParameter("name");
-		String userPassword = request.getParameter("pass");
+		// get the value through the parameter 
+		String name = request.getParameter("name");
+		String Password = request.getParameter("password");
 		
-		// Below the code for printing the data to the server
-		PrintWriter out  = response.getWriter();
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("userName", userName);
 		/**
-		 * This refer that get the request from the client and use the same servlet and show the books .
+		 * Below the code for httpsessions 
+		 * This will take the value from the cilent and send the different servlet
+		 * 
 		 */
+		HttpSession session = request.getSession();
+		session.setAttribute("userName", name);
+		
+		/**
+		 * Create a Printwriter for the show the data to the client
+		 *In servlet we can use the printwriter obj
+		 */
+		PrintWriter out = response.getWriter();
+		out.println("Email and pass valid");
+//		response.sendRedirect("home.html");
 		RequestDispatcher dispatcher = request.getRequestDispatcher("home.html");
-		dispatcher.forward(request, response); // Get the response and send the request to the client.
-		
-//		response.sendRedirect("home.html"); // This will redirect to that whole home html page and url will be changed.
-		
+		dispatcher.forward(request, response);
 	}
 
 }
